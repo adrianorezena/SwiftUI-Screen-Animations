e0f8adc51e5940db9b048ab9bcee27e6d48b74fa
adrianorezena <adrianocr@gmail.com>
2023-08-20 09:09:09 -0300

    Initial Commit

---
+++ animation1/animation1App.swift
@@ -0,0 +1,17 @@
+//
+//  animation1App.swift
+//  animation1
+//
+//  Created by Adriano Rezena on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct animation1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
